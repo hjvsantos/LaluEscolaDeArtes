fde89377fc97d3a7f8d2c357b273a28cf7e49849
João Lucas Santos <joao__lucas_santos@hotmail.com>
2015-08-12 19:17:18 -0300

    Criação das classes basicas. Falta implemetar!!!

---
+++ LaluEscolaDeArtes/src/ClassesBasicas/Aluno.java
@@ -0,0 +1,11 @@
+package ClassesBasicas;
+
+public class Aluno {
+	public String Nome;
+	public String Endereco;
+	public String NomeResponsavel;
+	public String CPF;
+	public String RG;
+	public String email;
+	
+}
